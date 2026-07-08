213727eeea901944af9c54b15bd91b1db9bb18f0
Md.Ashikur Rahman <ashikur@MdAshikurs-Mac-mini.local>
2026-07-09 00:39:26 +0600

    feat: added basic array indexing example

---
+++ array_intro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    //An array of 5 numbers
+    int number[] = {10, 20, 30, 40, 50};
+
+    // Way to view the first value of the array (index always starts at 0)
+    printf("First number is: %d\n", number[0]);
+    printf("Third number is: %d\n", number[2]);
+    
+    return 0;
+}
